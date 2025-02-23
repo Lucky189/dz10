f61fc869b99981f89023945fa06319a73650c0f0
Бордюжа Марк <borduzamark@gmail.com>
2025-02-23 23:39:22 +0200

    Add files via upload

---
+++ dz10.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+class Test
+{
+private:
+    int a = 1;
+    double b = 2.2;
+    char c = '!';
+
+    mutable bool isShowed = false;
+
+public:
+    void Enter()
+    {
+        isShowed = false;
+        std::cout << "Enter a: ";
+        std::cin >> a;
+        std::cout << "Enter b: ";
+        std::cin >> b;
+        std::cout << "Enter c: ";
+        std::cin >> c;
+    }
+
+    void Show() const
+    {
+        std::cout << "a = " << a << std::endl;
+        std::cout << "b = " << b << std::endl;
+        std::cout << "c = " << c << std::endl;
+    }
+};
+
+int main()
+{
+    const Test object;
+
+    object.Enter();
+    object.Show();
+        
+ }
